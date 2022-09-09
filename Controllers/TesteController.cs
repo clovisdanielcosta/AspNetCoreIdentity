9d9f4ff5ac97f59b76095421e8083af3f83b5a74
Clovis Costa <user@DESKTOP-SMC6CNC>
2022-09-09 10:53:06 -0300

    Introduction to Logger. Demonstration of KissLogger. Not implemented because intention to use Azure Telemetry.

---
+++ Controllers/TesteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentity.Controllers
+{
+    public class TesteController : Controller
+    {
+        private readonly ILogger<TesteController> _logger;
+
+        public TesteController(ILogger<TesteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogError("O erro aconteceu");
+            return View();
+        }
+    }
+}
